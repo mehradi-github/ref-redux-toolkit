8f91cc65454d62d4e049c6fa84c8c6eb713e5fe6
Mehradi <mehradi.info@gmail.com>
2023-09-21 11:08:28 +0200

    Upgrading project to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
